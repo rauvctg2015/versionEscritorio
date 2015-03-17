397089d02d1b2cbc96489e4c33734a023fb69fe5
rauvctg2015 <rauvctg@gmail.com>
2015-03-17 13:22:51 -0500

    completo
    completo

---
+++ testDevApp/testDevApp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace testDevApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblTitleForm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(100, 65);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "search File";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(63, 108);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(151, 120);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // lblTitleForm
+            // 
+            this.lblTitleForm.AutoSize = true;
+            this.lblTitleForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleForm.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblTitleForm.Location = new System.Drawing.Point(74, 21);
+            this.lblTitleForm.Name = "lblTitleForm";
+            this.lblTitleForm.Size = new System.Drawing.Size(136, 13);
+            this.lblTitleForm.TabIndex = 2;
+            this.lblTitleForm.Text = "PRUEBA JUNIOR DEV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lblTitleForm);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblTitleForm;
+    }
+}
+
---
+++ testDevApp/testDevApp/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testDevApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<string> resulList = new List<string>();
+            
+
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    
+                    this.richTextBox1.Visible = true;
+                    string content = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                    List<string> array = new List<string>();
+
+                    for (int i = 0; i < content.Length; i++)
+                    {
+                        if (content[i].ToString() != ",")
+                        {
+                            int intvariable;
+                            if (Int32.TryParse(content[i].ToString(), out intvariable) && i + 1 < content.Length)
+                            {
+                                if (Int32.TryParse(content[i + 1].ToString(), out intvariable))
+                                {
+                                    array.Add(content[i].ToString() + content[i + 1].ToString());
+                                    i = i + 1;
+                                }
+                                else
+                                {
+                                    array.Add(content[i].ToString());
+                                }
+                            }
+                        }
+
+                    }
+
+                    List<string> arrayFiltrado = new List<string>();
+                    foreach (var item in array)
+                    {
+                        if (Convert.ToInt32(item) % 5 != 0)
+                        {
+                            arrayFiltrado.Add(item);
+                        }
+                    }
+
+                    if (array.Count >= 1)
+                        {
+                            richTextBox1.Text = "Transpose: " + "\n\r";
+                            foreach (var item in arrayFiltrado)
+                            {
+                               int intVariable;
+                               if (Int32.TryParse(item, out intVariable))
+                               {
+
+                                    int firstNumber, secondNumber;
+                                    firstNumber = intVariable +5;
+                                    secondNumber = intVariable + 10;
+                                    if (this.richTextBox1.Text.Contains(item.Last()))
+                                    {
+                                        break;
+                                    }
+                                    if(Convert.ToInt32(item) < firstNumber && Convert.ToInt32(item) < secondNumber)
+                                    {
+                                        this.richTextBox1.Text += intVariable + ", " + (intVariable + 5) + ", " + (intVariable + 10); this.richTextBox1.Text += "\n\r";
+                                    }
+                               }
+                         
+                            }
+                        }
+                    else
+                    {
+                        MessageBox.Show("This file is Empty !!");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An Error was Unhandled: " + ex.Message + ex.StackTrace + ex.TargetSite.MethodHandle.ToString());
+                }
+                
+            }
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.richTextBox1.Visible = false;
+        }
+    }
+}
